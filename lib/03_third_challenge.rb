15950067cf8fb5e824366b21f27c4a68f9cb4c15
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-06 20:08:49 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -8,12 +8,12 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        :name "Lady Montague",
-        :age "54"
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
         #your key/value pairs here
-        
+        :name => 
         },
       :hero_friends => []
    }, 
